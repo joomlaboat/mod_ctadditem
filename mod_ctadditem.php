6d3b7c2f3a21a085d3724a96bebb8b699c9c803e
JoomlaBoat <ivankomlev@gmail.com>
2022-07-18 12:28:26 -0500

    Compatibility issue fixed.

--- mod_ctadditem.php
+++ mod_ctadditem.php
@@ -7,6 +7,9 @@
  **/
 
 // no direct access
+use CustomTables\CT;
+use CustomTables\CTUser;
+
 defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.html.pane');
@@ -43,9 +46,31 @@ if(!class_exists('ModCustomTablesViewEditItem'))
 	}
 }
 
-$config=array();
-$a=new ModCustomTablesViewEditItem;
+$path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_customtables'
+    . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'customtables' . DIRECTORY_SEPARATOR;
 
-$a->Model= JModelLegacy::getInstance('EditItem', 'CustomTablesModel', $config);
-$a->Model->load($params,true);
-$a->RenderForm();
+require_once($path.'loader.php');
+CTLoader();
+
+$config=array();
+
+$Model = JModelLegacy::getInstance('EditItem', 'CustomTablesModel', $config);
+
+$ct = new CT($params, true, $module->id);
+
+$Model->load($ct,true);
+
+if (!CTUser::CheckAuthorization($ct)) {
+    //not authorized
+    Factory::getApplication()->enqueueMessage(JoomlaBasicMisc::JTextExtended('COM_CUSTOMTABLES_NOT_AUTHORIZED'), 'error');
+    return false;
+}
+
+if (!isset($ct->Table->fields) or !is_array($ct->Table->fields))
+    return false;
+
+$formLink = $ct->Env->WebsiteRoot . 'index.php?option=com_customtables&amp;view=edititem' . ($ct->Params->ItemId != 0 ? '&amp;Itemid=' . $ct->Params->ItemId : '');
+if (!is_null($ct->Params->ModuleId))
+    $formLink .= '&amp;ModuleId=' . $ct->Params->ModuleId;
+
+CTViewEdit($ct, $Model->row, $Model->pagelayout, $formLink, 'eseditForm');
