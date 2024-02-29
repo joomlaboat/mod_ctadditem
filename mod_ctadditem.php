b5c880c6b8cb74f0aed3c88ec7965e76f5d0b14b
JoomlaBoat <ivankomlev@gmail.com>
2024-02-29 08:11:34 -0500

    Translations fixed

--- mod_ctadditem.php
+++ mod_ctadditem.php
@@ -16,27 +16,27 @@ defined('_JEXEC') or die('Restricted access');
 jimport('joomla.html.pane');
 
 $path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_customtables'
-    . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'customtables' . DIRECTORY_SEPARATOR;
+	. DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'customtables' . DIRECTORY_SEPARATOR;
 
-require_once($path.'loader.php');
+require_once($path . 'loader.php');
 CustomTablesLoader();
 
-$ct = new CT($params,true,$module->id);
+$ct = new CT($params, true, $module->id);
 
 if (!$ct->CheckAuthorization(1)) {
-    //not authorized
-    Factory::getApplication()->enqueueMessage(CTMiscHelper::JTextExtended('COM_CUSTOMTABLES_NOT_AUTHORIZED'), 'error');
-    return false;
+	//not authorized
+	Factory::getApplication()->enqueueMessage(CTMiscHelper::JTextExtended('COM_CUSTOMTABLES_NOT_AUTHORIZED'), 'error');
+	return false;
 }
 
 $tableName = $params['establename'];
 $ct->getTable($tableName);
 if (!isset($ct->Table->fields) or !is_array($ct->Table->fields))
-    return false;
+	return false;
 
 $formLink = $ct->Env->WebsiteRoot . 'index.php?option=com_customtables&amp;view=edititem' . ($ct->Params->ItemId != 0 ? '&amp;Itemid=' . $ct->Params->ItemId : '');
 //if (!is_null($ct->Params->ModuleId))
-    $formLink .= '&amp;ModuleId=' . $module->id;
+$formLink .= '&amp;ModuleId=' . $module->id;
 
 $editForm = new Edit($ct);
 $editForm->load();
